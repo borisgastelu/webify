e6f8825bf8e70b5c3438aa5a0c574efe44a5c274
borisgastelu <boris.gastelu@gmail.com>
2016-05-27 18:16:47 -0400

    sentencia for

---
+++ js/7.0.for.js
@@ -0,0 +1,21 @@
+var cant = prompt('Ingrese cantidad de iteraciones');
+cant = parseInt(cant);
+
+//  Inicializador ; Condicion ; Incrementador
+console.log('ASCENDENTE');
+for (var i = 0;    i < cant;   i++) {
+    console.log('Iteración #' + (i + 1));
+}
+
+console.log('DESCENDENTE');
+for (var i = cant; i > 0; i--) {
+    console.log('Iteración #' + i);
+}
+
+// for (var i = 0;    i < cant;   i += 1) {
+//     console.log('Iteración #' + (i + 1));
+// }
+//
+// for (var i = 1;    i <= cant;   i += 1) {
+//     console.log('Iteración #' + i);
+// }
---
+++ js/7.1.for.js
@@ -0,0 +1,25 @@
+var cant = prompt('Ingrese cantidad de iteraciones');
+cant = parseInt(cant);
+
+//  Inicializador
+var i = 0;
+
+//          ; Condicion ;
+for (       ; i < cant;      ) {
+
+    console.log('Iteración #' + (i + 1));
+
+    // Incrementador
+    i++;
+
+}
+
+//
+// while () {
+//     // instrucciones
+// }
+
+//
+// do () {
+//     // instrucciones
+// } while()
