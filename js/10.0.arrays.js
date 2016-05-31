7e4db1fd386acb74dfbbfb895d509e390e3f2538
borisgastelu <boris.gastelu@gmail.com>
2016-05-31 17:33:41 -0400

    implementando arrays

---
+++ js/10.0.arrays.js
@@ -0,0 +1,23 @@
+// Arrays: coleccion de elementos en una misma posicion de memoria ordenados secuencialmente
+//  Todos los arrays empiezan desde el indice 0 (posicion en la lista)
+
+var frutas = ['manzana', 'platano', 'naranja'];     // 3 elementos en el array
+// console.log(frutas[0]);
+// console.log(frutas[1]);
+// console.log(frutas[2]);
+
+var numeros = [1, 2, 3, 4, 5];  // array de numbers
+// console.log(numeros);
+
+var lista = ['hola', 1, 25.51, 'mundo', 'cruel'];   // array de diversos tipos
+// console.log(lista);
+
+var objetos = ['pantalla', 'teclado', 'parlante'];
+console.log(objetos);
+
+var number = 1;
+var string = 'Hola';
+var boolean = true;
+
+var tiposDatos = [number, string, boolean];
+// console.log(tiposDatos);
